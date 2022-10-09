0c6cd4c6f8be14c9806f599e07e16b5f099bf37a
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2022-10-09 13:46:32 +0000

    Punto 2

--- prova/src/app/container/container.component.ts
+++ prova/src/app/container/container.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
-  constructor() { }
+  macchine : string[];
+  constructor() {
+    this.macchine = ['escavatore','ruspa', 'rullo']
+   }
 
   ngOnInit(): void {
   }
